b22047ff48bb17f9895a7155966a12161938d573
fkkarakurt <fatihkkarakurt128@gmail.com>
2021-12-07 23:35:47 +0300

    CompressionMatrixFactory @class

---
+++ iNeural/CompressionMatrixFactory.h
@@ -0,0 +1,34 @@
+#ifndef INEURAL_COMPRESSION_MATRIX_FACTORY_H_
+#define INEURAL_COMPRESSION_MATRIX_FACTORY_H_
+
+#include <Eigen/Core>
+#include <vector>
+
+namespace iNeural
+{
+    class CompressionMatrixFactory
+    {
+    public:
+        bool compress;
+        enum Transformation
+        {
+            DCT,
+            GAUSSIAN,
+            SPARSE_RANDOM,
+            AVERAGE,
+            EDGE
+        } transformation;
+        int inputDim;
+        int paramDim;
+
+        CompressionMatrixFactory();
+        CompressionMatrixFactory(int inputDim, int paramDim,
+                                 Transformation transformation = DCT);
+        void createCompressionMatrix(Eigen::MatrixXd &cm);
+
+    private:
+        void fillCompressionMatrix(Eigen::MatrixXd &cm);
+    };
+} // namespace
+
+#endif
